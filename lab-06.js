55bea85b6639c4807a7925658a0ed87e2164adda
tstarti2 <167354176+tstarti2@users.noreply.github.com>
2024-05-10 12:43:18 -0700

    Added Book class and library function

---
+++ lab-06.js
@@ -0,0 +1,44 @@
+// Project: CIT 281 Lab 6
+// Author: Tyler Startin
+
+// create a Book class
+class Book {
+  constructor (title, author, pubDate) {
+    this.title = title
+    this.author = author
+    this.pubDate = pubDate
+  }
+}
+
+// create a book
+const atomicHabits = new Book('Atomic Habits', 'James Clear', '10/16/2018')
+
+class Library {
+  constructor (name) {
+    this._name = name
+    this._books = []
+  }
+
+  get books () {
+    return JSON.parse(JSON.stringify(this._books))
+  }
+
+  get count () {
+    return this._books.length
+  }
+
+  addBook (book = {}) {
+    const { title = '', author = '', pubDate = '' } = book
+    if (title.length > 0 && author.length > 0) {
+      const newBook = { title, author, pubDate }
+      this._books.push(newBook)
+    }
+  }
+
+  listBooks () {
+    for (const book of this._books) {
+      const { title, author, pubDate, isbn } = book
+      console.log(`Title: ${title}, Author: ${author}, PubDate: ${pubDate}`)
+    }
+  }
+}
