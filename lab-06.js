f6faa0b7aa4b62f987e8983de21732192dbe97c0
tstarti2 <167354176+tstarti2@users.noreply.github.com>
2024-05-10 13:47:06 -0700

    Added more books, now prints ISBN number

--- lab-06.js
+++ lab-06.js
@@ -3,10 +3,11 @@
 
 // create a Book class
 class Book {
-  constructor (title, author, pubDate) {
+  constructor (title, author, pubDate, isbn) {
     this.title = title
     this.author = author
     this.pubDate = pubDate
+    this.isbn = isbn
   }
 }
 
@@ -25,9 +26,9 @@ class Library {
   }
 
   addBook (book = {}) {
-    const { title = '', author = '', pubDate = '' } = book
+    const { title = '', author = '', pubDate = '', isbn = 'isbn' } = book
     if (title.length > 0 && author.length > 0) {
-      const newBook = { title, author, pubDate }
+      const newBook = { title, author, pubDate, isbn }
       this._books.push(newBook)
     }
   }
@@ -35,18 +36,21 @@ class Library {
   listBooks () {
     for (const book of this._books) {
       const { title, author, pubDate, isbn } = book
-      console.log(`Title: ${title}, Author: ${author}, PubDate: ${pubDate}`)
+      console.log(`Title: ${title}, Author: ${author}, PubDate: ${pubDate}, ISBN: ${isbn}`)
     }
   }
 }
 
 // test library class
-const library = new Library('New york Times Best Seller List')
+const library = new Library('New york Times Best Seller List');
 
-// create a book
-const atomicHabits = new Book('Atomic Habits', 'James Clear', '10/16/2018')
-
+// create a books
+const atomicHabits = new Book('Atomic Habits', 'James Clear', '10/16/2018', '0735211299');
+const kingOfSloth = new Book('King of Sloth', 'Ana Huang', '02/05/2024', '1728289750');
+const funnyStory = new Book('Funny Story', 'Emily Henry', '03/15/2024', '0593441281'); 
 // add book to library and show library contents
-library.addBook(atomicHabits)
-console.log(`Book Count: ${library.count}`)
-library.listBooks()
+library.addBook(atomicHabits);
+library.addBook(kingOfSloth);
+library.addBook(funnyStory);
+console.log(`Book Count: ${library.count}`);
+library.listBooks();
