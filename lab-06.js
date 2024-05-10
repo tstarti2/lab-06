1a83d5b6307801153a7a89f33dc256fe10052007
tstarti2 <167354176+tstarti2@users.noreply.github.com>
2024-05-10 14:27:22 -0700

    Added deleteBook. Deletes based on ISBN only

--- lab-06.js
+++ lab-06.js
@@ -26,11 +26,21 @@ class Library {
   }
 
   addBook (book = {}) {
-    const { title = '', author = '', pubDate = '', isbn = 'isbn' } = book
+    const { title = '', author = '', pubDate = '', isbn = '' } = book;
     if (title.length > 0 && author.length > 0) {
       const newBook = { title, author, pubDate, isbn }
       this._books.push(newBook)
     }
+  }
+
+  deleteBook (isbnNumber) {
+    const index = this._books.findIndex(book => book.isbn === isbnNumber);
+    
+    if (index === -1) {
+        console.log(`That book is not in the library`);
+    } else {
+        this._books.splice(index, 1);
+    }; 
   }
 
   listBooks () {
@@ -48,9 +58,13 @@ const library = new Library('New york Times Best Seller List');
 const atomicHabits = new Book('Atomic Habits', 'James Clear', '10/16/2018', '0735211299');
 const kingOfSloth = new Book('King of Sloth', 'Ana Huang', '02/05/2024', '1728289750');
 const funnyStory = new Book('Funny Story', 'Emily Henry', '03/15/2024', '0593441281'); 
-// add book to library and show library contents
+// add books to library and show library contents
 library.addBook(atomicHabits);
 library.addBook(kingOfSloth);
 library.addBook(funnyStory);
 console.log(`Book Count: ${library.count}`);
+library.listBooks();
+// delete a book from the library and show new contents
+console.log("* Library after delete *");
+library.deleteBook('0735211299');
 library.listBooks();
